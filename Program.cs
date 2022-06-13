7cba2af8d86b43d3e93a6d994d6958df21179360
a-kotynski <94733884+a-kotynski@users.noreply.github.com>
2022-06-13 06:19:17 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JuneThirteenth
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Think of a number: ");
+            int myNum = Convert.ToInt32(Console.ReadLine()); //it seems Console.ReadLine()
+                                                             //accepts user input as string or char
+            Console.WriteLine("Your number is: {0}", myNum);
+            Console.ReadLine();
+        }
+    }
+}
